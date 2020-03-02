556680d433d2b54aca10c7fa112e3f67c9034337
dc480506 <iamdev.dc@gmail.com>
2020-03-02 12:05:34 +0530

    Chatbot implementation done

--- a.py
+++
@@ -1,9 +0,0 @@
-import pickle
-some_list = [[1,2,3],[4,56]]
-
-#store data inside "some_file.p"
-pickle.dump( some_list, open( "some_file.p", "wb" ) )
-
-#load data from "some_file.p"
-other_list= pickle.load( open( "some_file.p", "rb" ) )
-print(other_list)
